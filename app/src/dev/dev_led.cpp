ccfe25e9ca3c8970089554545cbb0d4adc591aff
Yoon-Ki Hong <mymy49@nate.com>
2025-07-27 20:45:04 +0900

    누락된 라이선스 업데이트

--- app/src/dev/dev_led.cpp
+++ app/src/dev/dev_led.cpp
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2025 Yoon-Ki Hong
+ *
+ * This file is subject to the terms and conditions of the MIT License.
+ * See the file "LICENSE" in the main directory of this archive for more details.
+ */
+
 #include <dev/dev_led.h>
 #include <yss/instance.h>
 
